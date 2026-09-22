086e875a60dcc1e504992cadb204db9f5139477c
Kartonowy <ttrollface.com.pl@gmail.com>
2026-09-22 15:21:12 +0200

    new adapter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,26 @@
-<h1>Welcome to my website</h1>
+<script>
+    import { resolve } from "$app/paths";
+</script>
+<h2>Welcome to hacunae's website</h2>
+<p>There isn't much here yet but you are free to check out the <a href={resolve("/tierlist", {})} data-sveltekit-preload-data>tierlist</a> and the <a href={resolve("/tracker", {})} data-sveltekit-preload-data>tracker</a>.</p>
+
+<br>
+
+<p>With time there will be more and more things popping up here so you can visit once in a while!</p>
+
+<br>
+
+<p>I like if websites are Dark Reader friendly, hopefully I didn't flashbang you :)</p>
+
+<br>
+	
+<p>My other socials would be:<br>
+    <a href="https://github.com/Kartonowy">GitHub</a><br>
+    <a href="https://steamcommunity.com/id/zerothebitch">Steam</a><br>
+    <a href="https://myanimelist.net/profile/NullZweiChan">MyAnimeList</a><br>
+    <a href="https://letterboxd.com/nullzweichan/">Letterboxd</a><br>
+    <a href="https://www.goodreads.com/user/show/148668363-karton">Goodreads</a><br>
+    <a href="https://moxfield.com/users/2BLover">Moxfield</a><br>
+    <a href="https://myfigurecollection.net/profile/NullZweiChan">MyFigureCollection</a><br>
+    And if you wanna chat, hit me up on <a href="https://discord.com/users/no2typeb">discord</a>!<br>
+</p>
